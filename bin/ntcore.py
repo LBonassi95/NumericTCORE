26448f372519a62719d9071ab620958f35c452d1
LBonassi95 <l.bonassi005@unibs.it>
2023-02-27 11:03:32 +0100

    Added option for compiling away condeffs

--- bin/ntcore.py
+++ bin/ntcore.py
@@ -6,6 +6,7 @@ from unified_planning.engines.compilers.grounder import Grounder
 from numeric_tcore.compilation import NumericCompiler
 from unified_planning.model.fnode import FNode
 from unified_planning.model.walkers.state_evaluator import StateEvaluator
+from unified_planning.engines.compilers.conditional_effects_remover import ConditionalEffectsRemover
 import click
 import os
 
@@ -13,13 +14,18 @@ import os
 @click.argument('domain')
 @click.argument('problem')
 @click.argument('output')
-def main(domain, problem, output):
+@click.option('--compile_ce', is_flag=True, default=False)
+def main(domain, problem, output, compile_ce):
     reader = PDDLReader()
 
     problem = reader.parse_problem(domain, problem)
     compiler = NumericCompiler() 
     #tmp = CompilationKind.TRAJECTORY_CONSTRAINTS_REMOVING
     new_problem = compiler.compile(problem, CompilationKind.TRAJECTORY_CONSTRAINTS_REMOVING).problem
+
+    if compile_ce:
+        ce_remover = ConditionalEffectsRemover()
+        new_problem = ce_remover.compile(new_problem).problem
 
     writer = PDDLWriter(new_problem, needs_requirements=False)
     writer.write_domain(os.path.join(output, 'compiled_dom.pddl'))
